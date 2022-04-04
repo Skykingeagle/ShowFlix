ea5c8760533eddf79017325c41f9b7b8a9ec03f9
Skykingeagle <architmuni01@gmail.com>
2022-04-04 10:16:21 +0530

    showflixEmail

--- server-side/config/auth.config.js
+++ server-side/config/auth.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     secret: 'bezkoder-secret-key',
-    admin: 'host@email.com',  // host email id
-    pass: 'hostPass',  // host password
+    admin: 'showflixofficial6263@email.com',  // host email id
+    pass: 'Showflix6263',  // host password
     JWT_SECRET: 'hostSec',  // host Secret
 };
