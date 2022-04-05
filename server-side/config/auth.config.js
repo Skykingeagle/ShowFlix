406f54bb78ebcbce252cf026a598dc408222f0db
Pradeep <pradeepgupta36949@gmail.com>
2022-04-05 23:15:28 +0530

    checking the function of getOTP and changePass

--- server-side/config/auth.config.js
+++ server-side/config/auth.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     secret: 'bezkoder-secret-key',
-    admin: 'showflixofficial6263@email.com',  // host email id
+    admin: 'showflixofficial6263@gmail.com',  // host email id
     pass: 'Showflix6263',  // host password
-    JWT_SECRET: 'hostSec',  // host Secret
+    JWT_SECRET: 'secretToken',  // host Secret
 };
